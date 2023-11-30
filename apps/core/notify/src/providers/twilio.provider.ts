e6301c8b8c2778c292e857a8cd3435d6b98709a6
Brayden <bpofficial@icloud.com>
2023-12-01 06:46:38 +1000

    wip

--- apps/core/notify/src/providers/twilio.provider.ts
+++ apps/core/notify/src/providers/twilio.provider.ts
@@ -4,14 +4,9 @@ import {
     BulkSendRequest,
     BulkSendResponse,
 } from '@app/proto/notify';
-import { ConfigService } from '@nestjs/config';
 import { NotifyProvider } from '../types/notify-provider';
 
 export class TwilioProvider extends NotifyProvider {
-    constructor(configService: ConfigService) {
-        super(configService);
-    }
-
     send(data: SendRequest): Promise<SendResponse> {
         throw new Error('Method not implemented.');
     }
